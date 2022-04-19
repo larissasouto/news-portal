4a9a6640d646e03bb395a7564591d3a7831cb89a
Larissa Souto Del Rio <42869127+larissasouto@users.noreply.github.com>
2022-04-19 16:09:51 -0300

    paragraph component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Button from "./componentes/Button/Button";
 import Heading from "./componentes/Heading/Heading";
+import Paragraph from "./componentes/Paragraph/Paragraph";
 
 import "./App.css";
 
 function App() {
   return( 
-    <div>
+    <div className="container">
       <Button />
       <Heading title="Heading" />
+      <Paragraph />
     </div>
   );
 }
