334e6c7fde71729c5a29562a7c2e71d655a2b916
Larissa Souto Del Rio <42869127+larissasouto@users.noreply.github.com>
2022-04-19 12:23:48 -0300

    config files, button component

---
+++ src/componentes/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Button.scss';
+
+function Button() { 
+  return (
+    <button className="button">
+      <text className="button-label">
+        Button Label
+      </text>
+    </button>
+    
+  );
+}
+
+export default Button;
